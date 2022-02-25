fd6810ab17f836bda7bf0e88894abc6c7198209d
Chris <git@chris-nz.com>
2022-02-25 18:36:29 +1300

    de-cruft admin/groups command and remove incorrect reference to "mattata"

--- plugins/administration/groups.lua
+++ plugins/administration/groups.lua
@@ -5,15 +5,13 @@
 
 local groups = {}
 local mattata = require('mattata')
-local json = require('dkjson')
-local redis = require('libs.redis')
 
 function groups:init()
     groups.commands = mattata.commands(self.info.username):command('groups').table
-    groups.help = '/groups [query] - View a list of mattata\'s groups (that is, all of the ones added by the bot owner), or search for groups by passing a Lua pattern as a command argument.'
+    groups.help = '/groups [query] - View a list of the bot\'s groups (that is, all of the ones added by the bot owner), or search for groups by passing a Lua pattern as a command argument.'
 end
 
-function groups:on_message(message, configuration, language)
+function groups:on_message(message, configuration)
     local input = mattata.input(message.text)
     local output = {}
     for title, link in pairs(configuration.groups) do
@@ -24,7 +22,7 @@ function groups:on_message(message, configuration, language)
                 end
             )
             if not validate then
-                local output = 'Your search query contains a malformed Lua pattern! If you\'re not sure what this means, try searching without any symbols.'
+                output = 'Your search query contains a malformed Lua pattern! If you\'re not sure what this means, try searching without any symbols.'
                 return mattata.send_reply(message, output)
             end
         end
@@ -34,7 +32,7 @@ function groups:on_message(message, configuration, language)
         end
     end
     if not next(output) then
-        local output = 'No groups were found. If you\'d like your group to appear here, contact @flaunt_and_dither.'
+        output = 'No groups were found. If you\'d like your group to appear here, contact @flaunt_and_dither.'
         if input then
             output = string.format('No groups were found matching "%s"! Use /groups to view a complete list of available groups.', input)
         end
